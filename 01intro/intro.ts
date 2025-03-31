1e221275f7b7e515c321ff9696e90da127f52300
Aman Raj <amanraj6437@gmail.com>
2025-03-31 19:05:10 +0530

    first commit

---
+++ 01intro/intro.ts
@@ -0,0 +1,3 @@
+// run tsc <filename> to compile and convert  it into JS
+let myUser = { name: "Aman Raj", age: 28 };
+console.log(myUser);
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,9 @@
+let greetings: string = "Hello Aman";
+
+// greetings = 10 will give error
+
+let myNum: number = 10;
+
+// myNum.toUpperCase() will give error
+
+console.log(greetings);
