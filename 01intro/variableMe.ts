956e30fa6cccdc3d1de0b5f43b9cc2e82398ca44
Aman Raj <amanraj6437@gmail.com>
2025-03-31 20:36:24 +0530

    Variables

--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,9 +1,28 @@
+// string
 let greetings: string = "Hello Aman";
 
 // greetings = 10 will give error
 
+// number
 let myNum: number = 10;
 
 // myNum.toUpperCase() will give error
+
+//boolean
+let isLoggedIn: boolean = false;
+
+// dont use any
+let hero;
+
+function getHero() {
+  return "Thor";
+}
+hero = getHero();
+
+// ts in Functions
+function addTwo(num: number) {
+  return num + 1;
+}
+addTwo(5);
 
 console.log(greetings);
